822155844c43aaf30c46ca8b5e45a5f740d35f77
jo <chojh3123@gmail.com>
2025-10-28 22:50:55 +0900

    feature: jwt token 생성기 구현

---
+++ src/main/kotlin/com/github/healthverse/global/jwt/JwtTokenProvider.kt
@@ -0,0 +1,39 @@
+package com.github.healthverse.global.jwt
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.github.healthverse.domain.user.entity.User
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.oauth2.jwt.Jwt
+import org.springframework.stereotype.Component
+import java.time.LocalDateTime
+import java.util.*
+
+@Component
+class JwtTokenProvider(
+    @Value("\${jwt.secret}") private val secret: String
+) {
+    fun generateAccessToken(user: User): String {
+        val now = Date()
+        val expiry = Date(now.time + 1000L * 60 * 60) // 1시간
+        return JWT.create()
+            .withSubject(user.id.toString())
+            .withClaim("socialId", user.socialId)
+            .withClaim("socialType", user.socialType.name)
+            .withIssuedAt(now)
+            .withExpiresAt(expiry)
+            .sign(Algorithm.HMAC256(secret))
+    }
+
+    fun generateRefreshToken(user: User): String {
+        val now = Date()
+        val expiry = Date(now.time + 1000L * 60 * 60 * 24 * 7) // 7일
+        return JWT.create()
+            .withSubject(user.id.toString())
+            .withClaim("socialId", user.socialId)
+            .withClaim("socialType", user.socialType.name)
+            .withIssuedAt(now)
+            .withExpiresAt(expiry)
+            .sign(Algorithm.HMAC256(secret))
+    }
+}
